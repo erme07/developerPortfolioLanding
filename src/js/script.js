85d4ca6a184d07ba8b0dfbd6664fa52a21a7f472
erme07 <erik.medina07@gmail.com>
2024-11-26 16:06:01 -0300

    update

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,9 @@
+
+const $navbar = document.getElementById("navbar");
+const $navBarButton = document.getElementById("menu-button");
+const $menu = document.getElementById("menu");
+const $logo = document.getElementById("logo");
+
 const $clients = Array.from(document.querySelectorAll("[data-name='client']"));
 const $reviews = Array.from(document.querySelectorAll("[data-name='review']"));
 const $pointer = document.getElementById("pointer");
@@ -12,32 +18,31 @@ $customer.innerHTML="0+";
 $years.innerHTML="0+";
 $projects.innerHTML="0+";
 
+let posicionY = 0;
+
 let counter;
 let count = 0;
 let finish = 0;
 
+let new_client;
+let old_client;
+
 const options = {
-    root: null, // null significa que usa el viewport
-    rootMargin: '0px', // Margen alrededor del root
-    threshold: 0.15 // Umbral: 50% del elemento debe ser visible
+    root: null,
+    rootMargin: '0px',
+    threshold: 0.15
 };
 
 const observerCallback = (entries, observer) => {
     entries.forEach(entry => {
         if (entry.isIntersecting) {
-        // Acción cuando el elemento es visible
             counter = setInterval(increaseNumber,70)
-        } else {
-        // Acción cuando el elemento no es visible
-        }
+        } 
     });
 };
 
-// Crear el observer
 const observer = new IntersectionObserver(observerCallback, options);
 observer.observe($stats);
-
-
 
 const increaseNumber=()=>{
     count++;
@@ -51,12 +56,6 @@ const increaseNumber=()=>{
         clearInterval(counter)
     }
 }
-
-
-
-
-let new_client;
-let old_client;
 
 const resetcostumers = () => {
     $clients.forEach(item => {
@@ -101,6 +100,14 @@ const handleTransitionEnd = (e) => {
     }
 };
 
+const endTransitionMenu = (e) =>{
+    if(e.target == $menu && e.propertyName == "transform"){
+        $navbar.classList.add("bg-[rgb(0,0,0,.8)]","backdrop-blur-2xl")
+        $logo.classList.replace("opacity-0","opacity-100")
+        $menu.removeEventListener("transitionend", endTransitionMenu)
+    }
+}
+
 document.addEventListener('click', (e) =>{
     if(e.target.matches("[data-name='client'] img") && !(e.target.matches(".active-client img"))){
         let num_review = Number(e.target.getAttribute("data-number"));
@@ -112,11 +119,49 @@ document.addEventListener('click', (e) =>{
         else
             movePointer()
     }
-    if(e.target.id=="menu-button")
-        e.target.classList.toggle("show")
+    if(e.target == $navBarButton){
+        $navBarButton.classList.toggle("active")
+        $menu.classList.toggle("translate-y-0")
+        if($navBarButton.matches(".active")){
+            $navbar.classList.remove("bg-[rgb(0,0,0,.8)]","backdrop-blur-2xl")
+            $logo.classList.replace("opacity-100","opacity-0")
+        }
+        else if(scrollY!=0){
+            $menu.addEventListener("transitionend", endTransitionMenu)
+        }
+    }
 });
+
+document.addEventListener('scroll', (e) => {
+    if (posicionY > scrollY) { //subiendo
+        if(!$navBarButton.matches(".active")){
+            if(scrollY==0){
+                $navbar.classList.add("absolute")
+                $navbar.classList.remove("bg-[rgb(0,0,0,.8)]","backdrop-blur-2xl","transition-all");
+                $logo.classList.replace("opacity-100","opacity-0")
+            }else{
+                $navbar.classList.add("translate-y-0","transition-all");
+                $navbar.classList.remove("-translate-y-full");
+            }
+        }
+    }
+    else if(posicionY < scrollY){  //bajando
+        if(!$navBarButton.matches(".active")){
+            if (posicionY > 60) {
+                $navbar.classList.add("fixed","-translate-y-full","bg-[rgb(0,0,0,.8)]","backdrop-blur-2xl");
+                $navbar.classList.remove("absolute","translate-y-0")
+                $logo.classList.replace("opacity-0","opacity-100")
+            }
+        }
+    }
+    posicionY = scrollY;
+})
 
 screen.orientation.addEventListener("change", () => movePointer());
 window.addEventListener('resize', () => movePointer());
 
 movePointer()
+
+
+
+
